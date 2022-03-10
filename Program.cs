1d88963d3286e514c207d741d9daf695a85f6eb9
jeringeorge97 <101364266+jeringeorge97@users.noreply.github.com>
2022-03-11 02:33:22 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using dataproggassg2;
+
+Console.WriteLine("Press 1 to sign up\n" +
+    "Press 2 to Login\n" +
+    "Press 3 to view details");
+Console.WriteLine("Enter your option");
+
+int option = int.Parse(Console.ReadLine());
+if(option == 1)
+{
+    Json.Start(@"C:\Users\Kiran\Desktop\New folder\student.json");
+}
+        
+else if(option == 2)
+{
+    Login lg = new Login();
+    lg.login();
+
+}
+else if(option == 3)
+{
+    viewdetails vd = new viewdetails();
+    vd.read();
+
+}
+else
+{
+    Console.WriteLine("enter a valid choice");
+}
+        
+    
+        
+
+
+
+
+
+
+
+
+
